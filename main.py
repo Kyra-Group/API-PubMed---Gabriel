c58def684887fcbf6b006313c7b214952d434eb3
Gabriel Marina <gmarina@kyramadrid.es>
2024-11-29 17:30:19 +0100

    Solucionar problema de FastAPI con los ObjectID

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI, HTTPException
 from pubmeds import search_pubmed, fetch_details
 from mongodb import save_to_mongodb
+import traceback # Para saber qué da error
+from bson import ObjectId
+
+def serialize_article(article):
+    """
+    Convierte el ObjectId del campo '_id' a string.
+    """
+    if "_id" in article and isinstance(article["_id"], ObjectId):
+        article["_id"] = str(article["_id"])
+    return article
 
 app = FastAPI()
 
@@ -12,8 +22,20 @@ def read_root():
 async def search(termino_busqueda: str):
     try:
         ids = search_pubmed(termino_busqueda)
+        print("Lista de ids para subir a la bdd: ", ids)
         articles = fetch_details(ids)
-        save_to_mongodb(articles)
-        return {"message": "Artículos guardados en MongoDB", "articles": articles}
+        print("Artículos que se van a subir a la bdd: ", articles)
+        ids_insertados = save_to_mongodb(articles)
+        print("IDs insertados en MongoDB: ", ids_insertados)
+        articulos_serializados = [serialize_article(article) for article in articles]
+        return {
+            "message": "Artículos guardados en MongoDB",
+            "inserted_ids": ids_insertados,
+            "articles": articulos_serializados
+        }
+    
     except Exception as e:
+        # Imprime la traza completa del error
+        print("Error procesando la solicitud:")
+        traceback.print_exc()
         raise HTTPException(status_code=500, detail=str(e))
--- mongodb.py
+++ mongodb.py
@@ -4,6 +4,13 @@ client = MongoClient("mongodb://localhost:27017")
 db = client["pubmed_db"]
 
 def save_to_mongodb(articles: list):
+    if not isinstance(articles, list) or not articles:
+        raise ValueError("El argumento 'articles' debe ser una lista no vacía")
+
     collection = db["articles"]
-    result = collection.insert_many(articles)
-    return result.inserted_ids
+    try:
+        result = collection.insert_many(articles, ordered=False) # Insertar artículos en MongoDB
+        return [str(_id) for _id in result.inserted_ids] # Convertir ObjectId a string para que FastAPI pueda convertir las respuestas a JSON
+    except Exception as e:
+        print(f"Error al guardar en MongoDB: {e}")
+        return []
